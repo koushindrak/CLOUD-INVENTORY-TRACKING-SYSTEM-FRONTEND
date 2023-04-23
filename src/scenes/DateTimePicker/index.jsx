7853bb8a6b9832b09aa1832f6218d64aa497533d
koushindrak <127419668+koushindrak@users.noreply.github.com>
2023-04-23 02:40:03 -0400

    package.json dependencies added

---
+++ src/scenes/DateTimePicker/index.jsx
@@ -0,0 +1,24 @@
+import { Box, useTheme } from "@mui/material";
+import StaticDateTimePickerLandscape from "../../components/MyDateTimePicker"
+import Header from "../../components/Header";
+import { tokens } from "../../theme";
+
+const StaticDateTimePickerLandscape1 = () => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    return (
+        <Box m="20px">
+            <Header title="Date Time Picker" subtitle="Simple Date Time Picker " />
+
+            <Box
+                height="75vh"
+                border={`1px solid ${colors.grey[100]}`}
+                borderRadius="4px"
+            >
+                <StaticDateTimePickerLandscape />
+            </Box>
+        </Box>
+    );
+};
+
+export default StaticDateTimePickerLandscape1;
