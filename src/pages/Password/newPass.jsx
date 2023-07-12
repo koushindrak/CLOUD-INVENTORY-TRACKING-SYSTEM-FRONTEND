32b3e55b2fa8a9498c9f69a03d783904ad15bca2
kk1311 <86357435+kk1311@users.noreply.github.com>
2023-07-11 22:39:48 -0400

    Modified register user page

--- src/pages/Password/newPass.jsx
+++ src/pages/Password/newPass.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import TextField from '@mui/material/TextField';
@@ -9,142 +9,188 @@ import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import Alert from '@mui/material/Alert';
 import AlertTitle from '@mui/material/AlertTitle';
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { createGlobalStyle } from 'styled-components';
 
-function ResetPassword() {
-    const [newPassword, setNewPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
-    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
-    const [passwordError, setPasswordError] = useState(false);
-    const [resetError, setResetError] = useState(false);
-    const [resetSuccess, setResetSuccess] = useState(false);
+const ResetPassword = () => {
+  const [newPassword, setNewPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  const [resetError, setResetError] = useState(false);
+  const [resetSuccess, setResetSuccess] = useState(false);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleNewPasswordChange = (event) => {
-        setNewPassword(event.target.value);
-        setPasswordError(false);
-        setResetError(false);
-        setResetSuccess(false);
-    };
+  const GlobalStyle = createGlobalStyle`
+    body {
+      margin: 0;
+      padding: 0;
+      background: linear-gradient(to right, #8BC839, #F8B376, #34BEB7);
+    }
+  `;
 
-    const handleConfirmPasswordChange = (event) => {
-        setConfirmPassword(event.target.value);
-        setPasswordError(false);
-        setResetError(false);
-        setResetSuccess(false);
-    };
+  const handleNewPasswordChange = (event) => {
+    setNewPassword(event.target.value);
+    setPasswordError(false);
+    setResetError(false);
+    setResetSuccess(false);
+  };
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
+  const handleConfirmPasswordChange = (event) => {
+    setConfirmPassword(event.target.value);
+    setPasswordError(false);
+    setResetError(false);
+    setResetSuccess(false);
+  };
 
-        if (newPassword !== confirmPassword) {
-            setPasswordError(true);
-            setResetError(true);
-            return;
-        }
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-        // Simulating reset password request
-        // Replace this code with your actual reset password logic
-        setTimeout(() => {
-            setResetSuccess(true);
-        }, 2000);
-    };
+    if (newPassword !== confirmPassword) {
+      setPasswordError(true);
+      setResetError(true);
+      return;
+    }
 
-    const handleLoginNow = () => {
-        navigate('/login');
-        console.log('Login Now');
-    };
+    // Simulating reset password request
+    // Replace this code with your actual reset password logic
+    setTimeout(() => {
+      setResetSuccess(true);
+    }, 2000);
+  };
 
-    return (
-        <Container component="main" maxWidth="xs">
-            <Box
-                sx={{
-                    marginTop: 21,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    padding: '30px',
-                    border: '6px solid #ddd',
-                    borderRadius: '20px',
-                    backgroundColor: '#f5f5f5',
-                }}
+  const handleLoginNow = () => {
+    navigate('/login');
+    console.log('Login Now');
+  };
+
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#005EFF', // Adjust primary color as needed
+      },
+      secondary: {
+        main: '#111111', // Adjust secondary color as needed
+      },
+    },
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+        <GlobalStyle />
+          <Container component="main" maxWidth="xs">
+      <Box
+          sx={{
+            marginTop: 28,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            padding: '30px',
+            border: '2px solid #111',
+            borderRadius: '50px',
+            backgroundColor: '#ffffff',
+          }}
+        >
+          <Typography
+            component="h2"
+            variant="h7"
+            align="center"
+            sx={{
+              backgroundColor: '#111',
+              color: '#fff',
+              padding: '4px 10px',
+              borderRadius: '10px',
+              fontSize: '16px',
+              marginBottom: '5px',
+              marginTop: '9px'
+            }}
+          >
+            Reset Password
+          </Typography>
+          {resetError && (
+            <Alert severity="error" sx={{ mt: 2, mb: 2 }}>
+              <AlertTitle>Error</AlertTitle>
+              The passwords entered do not match. Please try again.
+            </Alert>
+          )}
+          {resetSuccess ? (
+            <>
+              <Alert severity="success" sx={{ mt: 2, mb: 2 }}>
+                <AlertTitle>Success</AlertTitle>
+                Your password has been successfully reset.
+              </Alert>
+              <Button
+              type="submit"
+              onClick={handleLoginNow}
+              fullWidth
+              variant="contained"
+              sx={{ mt: 1, mb: 1 }}
             >
-                <Typography component="h1" variant="h4" align="center">
-                    Password Reset
-                </Typography>
-                {resetError && (
-                    <Alert severity="error" sx={{mt: 2, mb: 2}}>
-                        <AlertTitle>Error</AlertTitle>
-                        The passwords entered do not match. Please try again.
-                    </Alert>
-                )}
-                {resetSuccess ? (
-                    <>
-                        <Alert severity="success" sx={{mt: 2, mb: 2}}>
-                            <AlertTitle>Success</AlertTitle>
-                            Your password has been successfully reset.
-                        </Alert>
-                        <Button
-                            onClick={handleLoginNow}
-                            fullWidth
-                            variant="contained"
-                            sx={{mt: 2, mb: 2, backgroundColor: '#0362fc', color: 'white'}}
-                        >
-                            Login Now
-                        </Button>
-                    </>
-                ) : (
-                    <form onSubmit={handleSubmit} noValidate>
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            name="newPassword"
-                            label="New Password"
-                            type="password"
-                            id="newPassword"
-                            autoComplete="new-password"
-                            value={newPassword}
-                            onChange={handleNewPasswordChange}
-                            error={passwordError}
-                            helperText={passwordError ? "Passwords don't match" : ''}
-                        />
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            name="confirmPassword"
-                            label="Confirm Password"
-                            type={showConfirmPassword ? 'text' : 'password'}
-                            id="confirmPassword"
-                            autoComplete="new-password"
-                            value={confirmPassword}
-                            onChange={handleConfirmPasswordChange}
-                            error={passwordError}
-                            helperText={passwordError ? "Passwords don't match" : ''}
-                            InputProps={{
-                                endAdornment: (
-                                    <IconButton onClick={() => setShowConfirmPassword((prevShow) => !prevShow)}
-                                                edge="end">
-                                        {showConfirmPassword ? <VisibilityOff/> : <Visibility/>}
-                                    </IconButton>
-                                ),
-                            }}
-                        />
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            sx={{mt: 3, mb: 2, backgroundColor: '#32ba91', color: 'white'}}
-                        >
-                            Reset Password
-                        </Button>
-                    </form>
-                )}
-            </Box>
-        </Container>
-    );
+              Login Now
+            </Button>
+            </>
+          ) : (
+            <form onSubmit={handleSubmit} noValidate>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="newPassword"
+                label="New Password"
+                type="password"
+                id="newPassword"
+                autoComplete="new-password"
+                value={newPassword}
+                onChange={handleNewPasswordChange}
+                error={passwordError}
+                helperText={passwordError ? "Passwords don't match" : ''}
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="confirmPassword"
+                label="Confirm Password"
+                type={showConfirmPassword ? 'text' : 'password'}
+                id="confirmPassword"
+                autoComplete="new-password"
+                value={confirmPassword}
+                onChange={handleConfirmPasswordChange}
+                error={passwordError}
+                helperText={passwordError ? "Passwords don't match" : ''}
+                InputProps={{
+                  endAdornment: (
+                    <IconButton
+                      onClick={() =>
+                        setShowConfirmPassword((prevShow) => !prevShow)
+                      }
+                      edge="end"
+                    >
+                      {showConfirmPassword ? (
+                        <VisibilityOff />
+                      ) : (
+                        <Visibility />
+                      )}
+                    </IconButton>
+                  ),
+                }}
+              />
+              <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 1, mb: 1 }}
+            >
+              Reset Password
+            </Button>
+            </form>
+          )}
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
 }
 
 export default ResetPassword;
