c96f428bca113ba720a03a8c75616eadd7f26aae
koushindrak <127419668+koushindrak@users.noreply.github.com>
2023-07-16 15:30:09 +0530

    horizontal list added

--- src/pages/ComponentsSourcing/index.jsx
+++ src/pages/ComponentsSourcing/index.jsx
@@ -5,15 +5,12 @@ import {useDispatch, useSelector} from 'react-redux';
 import 'react-toastify/dist/ReactToastify.css';
 import {getSuggestedComponent, getSuggestedComponentSuccess} from "./GetAllSuggestedComponent";
 import {getSuggestedComponentById, getSuggestedComponentByIdSuccess} from "./GetSuggestedComponentById";
-import { Card, CardContent, Typography, CardHeader, CardActions } from '@mui/material';
+import { Card, CardContent, Typography, CardHeader, CardActions,Paper } from '@mui/material';
 import './CardsStyle.css'; // import the new CSS file
-
 const SuggestedComponent = () => {
     const getByPartNumberSuccessRes = useSelector(getSuggestedComponentByIdSuccess);
     const [suggestedComp, setSuggestedComp] = useState(null);
     const dispatch = useDispatch();
-
-    const theme = useTheme();
 
     useEffect(() => {
         dispatch(getSuggestedComponentById("2056-GRILLEFR87-ND"));
@@ -27,10 +24,11 @@ const SuggestedComponent = () => {
 
     return (
         <Box marginLeft="275px">
-            <Typography variant="h4" style={{color: theme.palette.primary.main}}>Product Information</Typography>
+            <Typography variant="h4" className="header">Product Information</Typography>
             { suggestedComp &&
-                <Card className="card">
+                <Paper className="card" elevation={3}>
                     <CardHeader
+                        className="card-header"
                         avatar={
                             <Avatar
                                 className="avatar"
@@ -38,11 +36,11 @@ const SuggestedComponent = () => {
                                 src={suggestedComp.Product.PrimaryPhoto}
                             />
                         }
-                        title={<Typography variant="h6" className="title">{suggestedComp.Product.ProductDescription}</Typography>}
-                        subheader={<Typography variant="subtitle1" className="subheader">Manufacturer: {suggestedComp.Product.Manufacturer.Value}</Typography>}
+                        title={suggestedComp.Product.ProductDescription}
+                        subheader={`Manufacturer: ${suggestedComp.Product.Manufacturer.Value}`}
                     />
                     <CardContent>
-                        <Typography variant="body2" className="body-text" component="p">
+                        <Typography variant="body2" component="p">
                             Unit Price: {suggestedComp.Product.UnitPrice} <br/>
                             Available Quantity: {suggestedComp.Product.QuantityAvailable} <br/>
                             Packaging: {suggestedComp.Product.Packaging.Value} <br/>
@@ -51,20 +49,21 @@ const SuggestedComponent = () => {
                             Category: {suggestedComp.Product.Category.Value} <br/>
                         </Typography>
                         <CardActions disableSpacing>
-                            <Button size="small" style={{color: theme.palette.secondary.main}} href={suggestedComp.Product.PrimaryDatasheet}>
+                            <Button size="small" color="primary" href={suggestedComp.Product.PrimaryDatasheet}>
                                 View Datasheet
                             </Button>
                         </CardActions>
                     </CardContent>
-                </Card>
+                </Paper>
             }
-            <Typography variant="h4" style={{color: theme.palette.primary.main, marginTop: '30px'}}>Suggested Products</Typography>
+            <Typography variant="h4" className="header" style={{marginTop: '30px'}}>Suggested Products</Typography>
             {suggestedComp && suggestedComp.SuggestedProducts &&
                 <Grid container spacing={3}>
                     { suggestedComp.SuggestedProducts.map((product, index) => (
                         <Grid item xs={12} sm={6} key={index}>
-                            <Card className="card">
+                            <Paper className="card" elevation={3}>
                                 <CardHeader
+                                    className="card-header"
                                     avatar={
                                         <Avatar
                                             className="avatar"
@@ -72,11 +71,11 @@ const SuggestedComponent = () => {
                                             src={product.PrimaryPhoto}
                                         />
                                     }
-                                    title={<Typography variant="h6" className="title">{product.ProductDescription}</Typography>}
-                                    subheader={<Typography variant="subtitle1" className="subheader">Manufacturer: {product.Manufacturer.Value}</Typography>}
+                                    title={product.ProductDescription}
+                                    subheader={`Manufacturer: ${product.Manufacturer.Value}`}
                                 />
                                 <CardContent>
-                                    <Typography variant="body2" className="body-text" component="p">
+                                    <Typography variant="body2" component="p">
                                         Unit Price: {product.UnitPrice} <br/>
                                         Available Quantity: {product.QuantityAvailable} <br/>
                                         Packaging: {product.Packaging.Value} <br/>
@@ -85,12 +84,12 @@ const SuggestedComponent = () => {
                                         Category: {product.Category.Value} <br/>
                                     </Typography>
                                     <CardActions disableSpacing>
-                                        <Button size="small" style={{color: theme.palette.secondary.main}} href={product.PrimaryDatasheet}>
+                                        <Button size="small" color="primary" href={product.PrimaryDatasheet}>
                                             View Datasheet
                                         </Button>
                                     </CardActions>
                                 </CardContent>
-                            </Card>
+                            </Paper>
                         </Grid>
                     ))}
                 </Grid>
