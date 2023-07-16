6ef836c8e4557b77df084c5704a28e422ce3f764
koushindrak <127419668+koushindrak@users.noreply.github.com>
2023-07-17 02:55:03 +0530

    loader added

--- src/pages/ComponentsSourcing/index.jsx
+++ src/pages/ComponentsSourcing/index.jsx
@@ -59,74 +59,96 @@ const SuggestedComponent = () => {
                     style={{ marginBottom: '20px', width: '300px' }}
                 />
                 {!loading && suggestedComp ? (
-                <>
-                    <Typography variant="h4" className="header">Product Information</Typography>
-                    <Paper className="card" elevation={3}>
-                        <CardHeader
-                            className="card-header"
-                            avatar={
-                                <Avatar
-                                    className="avatar"
-                                    aria-label="product"
-                                    src={suggestedComp.Product.PrimaryPhoto}
-                                />
-                            }
-                            title={suggestedComp.Product.ProductDescription}
-                            subheader={`Manufacturer: ${suggestedComp.Product.Manufacturer.Value}`}
-                        />
-                        <CardContent>
-                            <Typography variant="body2" component="p">
-                                Unit Price: {suggestedComp.Product.UnitPrice} <br />
-                                Available Quantity: {suggestedComp.Product.QuantityAvailable} <br />
-                                Packaging: {suggestedComp.Product.Packaging.Value} <br />
-                                Series: {suggestedComp.Product.Series.Value} <br />
-                                Manufacturer Lead Weeks: {suggestedComp.Product.ManufacturerLeadWeeks} <br />
-                                Category: {suggestedComp.Product.Category.Value} <br />
-                            </Typography>
-                            <CardActions disableSpacing>
-                                <Button size="small" color="primary" href={suggestedComp.Product.PrimaryDatasheet}>
-                                    View Datasheet
-                                </Button>
-                            </CardActions>
-                        </CardContent>
-                    </Paper>
-                    <Typography variant="h4" className="header" style={{ marginTop: '30px' }}>Suggested Products</Typography>
-                    <Grid container spacing={3}>
-                        {suggestedComp.SuggestedProducts.map((product, index) => (
-                            <Grid item xs={12} sm={6} key={index}>
-                                <Paper className="card" elevation={3}>
-                                    <CardHeader
-                                        className="card-header"
-                                        avatar={
-                                            <Avatar
-                                                className="avatar"
-                                                aria-label="product"
-                                                src={product.PrimaryPhoto}
-                                            />
-                                        }
-                                        title={product.ProductDescription}
-                                        subheader={`Manufacturer: ${product.Manufacturer.Value}`}
+                    <>
+                        <Typography variant="h4" className="header">Product Information</Typography>
+                        <Paper className="card" elevation={3}>
+                            <CardHeader
+                                className="card-header"
+                                avatar={
+                                    <Avatar
+                                        className="avatar"
+                                        aria-label="product"
+                                        src={suggestedComp.Product.PrimaryPhoto}
                                     />
-                                    <CardContent>
-                                        <Typography variant="body2" component="p">
-                                            Unit Price: {product.UnitPrice} <br />
-                                            Available Quantity: {product.QuantityAvailable} <br />
-                                            Packaging: {product.Packaging.Value} <br />
-                                            Series: {product.Series.Value} <br />
-                                            Manufacturer Lead Weeks: {product.ManufacturerLeadWeeks} <br />
-                                            Category: {product.Category.Value} <br />
-                                        </Typography>
-                                        <CardActions disableSpacing>
-                                            <Button size="small" color="primary" href={product.PrimaryDatasheet}>
-                                                View Datasheet
-                                            </Button>
-                                        </CardActions>
-                                    </CardContent>
-                                </Paper>
-                            </Grid>
-                        ))}
-                    </Grid>
-                </>
+                                }
+                                title={suggestedComp.Product.ProductDescription}
+                                subheader={`Manufacturer: ${suggestedComp.Product.Manufacturer.Value}`}
+                            />
+                            <CardContent>
+                                <Typography variant="body2" component="p">
+                                    Obsolete: {suggestedComp.Product.Obsolete?.toString() ?? 'N/A'} <br />
+                                    Unit Price: {suggestedComp.Product.UnitPrice} <br />
+                                    Available Quantity: {suggestedComp.Product.QuantityAvailable} <br />
+                                    Product URL: <a href={suggestedComp.Product.ProductUrl}>{suggestedComp.Product.ProductUrl}</a> <br />
+                                    Primary Photo: <a href={suggestedComp.Product.PrimaryPhoto}>{suggestedComp.Product.PrimaryPhoto}</a> <br />
+                                    Manufacturer Page URL: <a href={suggestedComp.Product.ManufacturerPageUrl}>{suggestedComp.Product.ManufacturerPageUrl}</a> <br />
+                                    Category: {suggestedComp.Product.Category.Value} <br />
+                                    Manufacturer Part Number: {suggestedComp.Product.ManufacturerPartNumber} <br />
+                                    Minimum Order Quantity: {suggestedComp.Product.MinimumOrderQuantity} <br />
+                                    NonStock: {suggestedComp.Product.NonStock?.toString() ?? 'N/A'} <br />
+                                    Quantity Available: {suggestedComp.Product.QuantityAvailable} <br />
+                                    DigiKey Part Number: {suggestedComp.Product.DigiKeyPartNumber} <br />
+                                    Product Description: {suggestedComp.Product.ProductDescription} <br />
+                                    Unit Price: {suggestedComp.Product.UnitPrice} <br />
+                                    Manufacturer: {suggestedComp.Product.Manufacturer.Value} <br />
+                                    Manufacturer Public Quantity: {suggestedComp.Product.ManufacturerPublicQuantity} <br />
+                                    Supplier: {suggestedComp.Product.Supplier} <br />
+                                </Typography>
+                                <CardActions disableSpacing>
+                                    <Button size="small" color="primary" href={suggestedComp.Product.PrimaryDatasheet}>
+                                        View Datasheet
+                                    </Button>
+                                </CardActions>
+                            </CardContent>
+                        </Paper>
+                        <Typography variant="h4" className="header" style={{ marginTop: '30px' }}>Suggested Products</Typography>
+                        <Grid container spacing={3}>
+                            {suggestedComp.SuggestedProducts.map((product, index) => (
+                                <Grid item xs={12} sm={6} key={index}>
+                                    <Paper className="card" elevation={3}>
+                                        <CardHeader
+                                            className="card-header"
+                                            avatar={
+                                                <Avatar
+                                                    className="avatar"
+                                                    aria-label="product"
+                                                    src={product.PrimaryPhoto}
+                                                />
+                                            }
+                                            title={product.ProductDescription}
+                                            subheader={`Manufacturer: ${product.Manufacturer.Value}`}
+                                        />
+                                        <CardContent>
+                                            <Typography variant="body2" component="p">
+                                                Obsolete: {product.Obsolete?.toString() ?? 'N/A'} <br />
+                                                Unit Price: {product.UnitPrice} <br />
+                                                Available Quantity: {product.QuantityAvailable} <br />
+                                                Product URL: <a href={product.ProductUrl}>{product.ProductUrl}</a> <br />
+                                                Primary Photo: <a href={product.PrimaryPhoto}>{product.PrimaryPhoto}</a> <br />
+                                                Manufacturer Page URL: <a href={product.ManufacturerPageUrl}>{product.ManufacturerPageUrl}</a> <br />
+                                                Category: {product.Category.Value} <br />
+                                                Manufacturer Part Number: {product.ManufacturerPartNumber} <br />
+                                                Minimum Order Quantity: {product.MinimumOrderQuantity} <br />
+                                                NonStock: {product.NonStock?.toString() ?? 'N/A'} <br />
+                                                Quantity Available: {product.QuantityAvailable} <br />
+                                                DigiKey Part Number: {product.DigiKeyPartNumber} <br />
+                                                Product Description: {product.ProductDescription} <br />
+                                                Unit Price: {product.UnitPrice} <br />
+                                                Manufacturer: {product.Manufacturer.Value} <br />
+                                                Manufacturer Public Quantity: {product.ManufacturerPublicQuantity} <br />
+                                                Supplier: {product.Supplier} <br />
+                                            </Typography>
+                                            <CardActions disableSpacing>
+                                                <Button size="small" color="primary" href={product.PrimaryDatasheet}>
+                                                    View Datasheet
+                                                </Button>
+                                            </CardActions>
+                                        </CardContent>
+                                    </Paper>
+                                </Grid>
+                            ))}
+                        </Grid>
+                    </>
                 ) : (!firstLoad && !loading ? (
                     <Box
                         display="flex"
