a7dba88509d261408924fd38425250dbca67f820
koushindrak <127419668+koushindrak@users.noreply.github.com>
2023-07-17 06:51:03 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,1 @@
-export const API_HOST = "http://backend-service.default.svc.cluster.local:2612";
+export const API_HOST = "http://adde172c0806047e89f8b4ec37f572ec-837538779.us-east-1.elb.amazonaws.com";
