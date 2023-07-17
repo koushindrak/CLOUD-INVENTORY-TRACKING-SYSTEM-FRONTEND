da4371ce4e5825f01379b9d22693a5f659f65901
koushindrak <127419668+koushindrak@users.noreply.github.com>
2023-07-17 07:49:34 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,1 @@
-export const API_HOST = "http://adde172c0806047e89f8b4ec37f572ec-837538779.us-east-1.elb.amazonaws.com";
+export const API_HOST = "http://adde172c0806047e89f8b4ec37f572ec-837538779.us-east-1.elb.amazonaws.com:2612";
