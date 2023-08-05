ab5ae8136529b32e8e17e872370ba8ff29943570
saumyak05 <128255046+saumyak05@users.noreply.github.com>
2023-08-05 10:35:06 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
 // export const API_HOST = process.env.REACT_APP_BACKEND_URL || "http://localhost:2612";
-export const API_HOST = "http://a12a638ddd18c4f828bbf895fa2cb851-1233678057.us-east-1.elb.amazonaws.com:2612";
+export const API_HOST = "http://acb220dbd17124b66930d1dab48bfdab-875053215.us-east-1.elb.amazonaws.com:2612";
