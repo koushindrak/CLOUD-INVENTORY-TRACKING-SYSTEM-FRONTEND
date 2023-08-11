bc204c744494f4e4d712913c7e4631dc0c3dcf49
koushindrak <127419668+koushindrak@users.noreply.github.com>
2023-08-11 10:35:48 -0400

    resolved

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const API_HOST = process.env.REACT_APP_BACKEND_URL || "http://a12a638ddd18c4f828bbf895fa2cb851-1233678057.us-east-1.elb.amazonaws.com:2612";
-// export const API_HOST = "http://a12a638ddd18c4f828bbf895fa2cb851-1233678057.us-east-1.elb.amazonaws.com:2612";
+// export const API_HOST = process.env.REACT_APP_BACKEND_URL || "http://a12a638ddd18c4f828bbf895fa2cb851-1233678057.us-east-1.elb.amazonaws.com:2612";
+export const API_HOST = "a580e02fe06bd42458afc3b133ae0a92-189480624.us-east-1.elb.amazonaws.com:2612";
 
 //REACT_APP_BACKEND_URL=http://localhost:2612 npm start
